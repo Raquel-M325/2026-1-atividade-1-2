c24420eceb9414b59549e31e923edb485000ec52
Miguel Rodrigues Spínola da Hora <mrsdahora@gmail.com>
2026-05-21 12:01:04 +0000

    Mais um pouco do server com threads

--- servidor/servert.lua
+++ servidor/servert.lua
@@ -8,3 +8,20 @@ local function dar_boas_vindas(client_id)
     cliente:settimeout(10)
 
     print(string.format("[Threads-%s] Cliente conectado!", tostring(client_id)))
+
+    cliente:send("Digite algo para sair do server")
+
+    while true do
+        local linha, erro = cliente:recive()
+
+        if erro then
+            print(string.format("Conexão perdida", client_id, erro))
+            break
+        end
+
+        print("A pessoinha enviou %s", client_id, linha)
+
+        if linha == "sair" then
+            cliente:send("Até logo!!!")
+            break
+        end
